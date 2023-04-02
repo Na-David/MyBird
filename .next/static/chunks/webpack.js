3e74d8812232ad475c97d2e8e7b1a4b04dc8df80
David Na <hna206@naver.com>
2023-04-03 00:07:32 +1200

    cache memory

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "834ea82a5fbfc93297e8";
+/******/ 	var hotCurrentHash = "2d6580b645c571429578";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import {Form, Input, Button} from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
@@ -19,6 +19,7 @@ const LoginForm = () => {
     const onChangePassword = useCallback((e) => {
         setPassword(e.target.value);
     },[])
+    
 
 
   return (
