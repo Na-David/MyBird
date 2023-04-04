10310b0a6960d8f63bffa12b5924a8142e9002e1
David Na <hna206@naver.com>
2023-04-05 01:56:59 +1200

    list ---ing

--- components/FollowingList.jsx
+++ components/FollowingList.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {List, Button} from 'antd';
 
-const FollowingList = () => {
+const FollowingList = ({header, data}) => {
   return (
-    <div>
-      
-    </div>
+    <List 
+        style={{marginBottom: 20}}
+        grid={{gutter: 4, xs: 2, md: 3}}
+        size="small"
+        header = {<div>{header}</div>}
+        loadMore = {<div style={{textAlign: 'center', margin: '10px 0'}}><Button>Load More</Button></div>}
+        bordered
+        dataSource = {data}
+        renderItem={(item) => (
+            <List.Item style = {{marginTop: 20}}></List.Item>
+        )}
+    />
   )
 }
 
