7f8889325e9c98f7d9b5493873b11e2e7826180a
David Na <hna206@naver.com>
2023-04-10 00:50:58 +1200

    configure -- redux --> wrapper

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import 'antd/dist/reset.css'
-import Head from 'next/head';
+import React from "react";
+import PropTypes from "prop-types";
+import "antd/dist/reset.css";
+import Head from "next/head";
+import wrapper from "../store/configureStore";
 
-const MyBird = ({Component}) => {
+const MyBird = ({ Component }) => {
   return (
     <>
-    <Head>
-        <meta charSet='utf-8' />
+      <Head>
+        <meta charSet="utf-8" />
         <title>My--Bird</title>
-    </Head>
-        <Component />
+      </Head>
+      <Component />
     </>
-  )
+  );
 };
 
 MyBird.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-}
+  Component: PropTypes.elementType.isRequired,
+};
 
-export default MyBird;
+export default wrapper.withRedux(MyBird);
