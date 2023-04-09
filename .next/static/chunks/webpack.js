11469eebd736334a3d3a92d5a1b31bb0e652b015
David Na <hna206@naver.com>
2023-04-09 23:59:18 +1200

    terms

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "35a6c6965e1d38ef75e7";
+/******/ 	var hotCurrentHash = "2878b0a035ce2cc26bda";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,78 +1,102 @@
-import React, { useCallback, useState } from 'react'
-import AppLayout from '../components/AppLayout';
-import Head from 'next/head';
-import { Form, Input} from 'antd';
-import styled from 'styled-components';
-import useInput from '../hooks/useInput';
+import React, { useCallback, useState } from "react";
+import AppLayout from "../components/AppLayout";
+import Head from "next/head";
+import { Form, Input, Checkbox, Button } from "antd";
+import styled from "styled-components";
+import useInput from "../hooks/useInput";
 
 const ErrorMessage = styled.div`
   color: red;
 `;
 
-
 const SignUp = () => {
-
-  const [id, onChangeId] = useInput('');
+  const [id, onChangeId] = useInput("");
   // const [id, setId] = useState('');
   // const onChangeId = useCallback((e) => {
   //   setId(e.target.value);
   // },[])
-  const [nickname, onChangeNickname] = useInput('');
+  const [nickname, onChangeNickname] = useInput("");
   // const [nickname, setNickname] = useState('');
   // const onChangeNickname = useCallback((e) => {
   //   setNickname(e.target.value);
   // },[])
-  const [password, onChangePassword] = useInput('');
+  const [password, onChangePassword] = useInput("");
   // const [password, setPassword] = useState('');
   // const onChangePassword = useCallback((e) => {
   //   setPassword(e.target.value);
   // },[])
 
-  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState("");
   const [passwordError, setPasswordError] = useState(false);
-  const onChangePasswordCheck = useCallback((e) => {
-    setPasswordCheck(e.target.value);
-    setPasswordError(e.target.value !== password);
-  },[password])
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
 
-  const onSubmit = useCallback(() => {
-    
-  },[])
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const onSubmit = useCallback(() => {}, []);
 
   return (
     <AppLayout>
-    <Head>
-      <title>Sign Up | My Bird</title>
-    </Head>
-    <Form onFinish={onSubmit}>
-      <div>
-        <label htmlFor='user-id'>ID: </label> <br />
-        <Input name='user-id' value={id} required onChange={onChangeId} />
-      </div>
-      <div>
-        <label htmlFor='user-nick'>Nickname: </label> <br />
-        <Input name='user-nick' value={nickname} required onChange={onChangeNickname} />
-      </div>
-      <div>
-        <label htmlFor='user-password'>Password: </label><br />
-        <Input name='user-password' type='password' value={password} required onChange={onChangePassword} />
-      </div>
-      <div>
-        <label htmlFor='user-password-check'>Check the Password</label> <br />
-        <Input 
-          name = 'user-password-check'
-          type= 'password'
-          value={passwordCheck}
-          required
-          onChange={onChangePasswordCheck}
-        />
-        {passwordError && <ErrorMessage>Wrong Password</ErrorMessage>}
-      </div>
-
-
-    </Form>
+      <Head>
+        <title>Sign Up | My Bird</title>
+      </Head>
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor="user-id">ID: </label> <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">Nickname: </label> <br />
+          <Input
+            name="user-nick"
+            value={nickname}
+            required
+            onChange={onChangeNickname}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">Password: </label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">Check the Password</label> <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && <ErrorMessage>Wrong Password</ErrorMessage>}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            I will be happy
+          </Checkbox>
+          {termError && (
+            <ErrorMessage>You need to agree above term.</div>
+          )}
+        </div>
+      </Form>
     </AppLayout>
-  )
-}
+  );
+};
 
 export default SignUp;
