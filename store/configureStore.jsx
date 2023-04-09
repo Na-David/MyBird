f959c22a032e50b9e482c4736933c0bae01987ad
David Na <hna206@naver.com>
2023-04-10 01:00:36 +1200

    reducer, enhancer

--- store/configureStore.jsx
+++ store/configureStore.jsx
@@ -1,6 +1,10 @@
 import { createWrapper } from "next-redux-wrapper";
+import { createStore } from "redux";
 
-const configureStore = () => {};
+const configureStore = () => {
+  const store = createStore(reducer, enhancer);
+  return store;
+};
 
 const wrapper = createWrapper(configureStore, {
   debug: process.env.NODE_ENV === "development",
